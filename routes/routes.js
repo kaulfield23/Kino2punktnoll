53f8209d5aac127734016af1a879bbf546798b46
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-01-30 21:50:50 +0100

    added post form

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@ import home from "./home.js";
 import movies from "./movies.js";
 import contact from "./contact.js";
 import covidinformation from "./covidinformation.js";
+import postReviews from "./postReviews.js";
 
 export default {
   home,
   movies,
   contact,
   covidinformation,
+  postReviews,
 };
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { engine } from "express-handlebars";
 import { marked } from "marked";
 import routes from "../routes/routes.js";
+import  fetch  from 'node-fetch';
 
 const app = express();
 
@@ -16,11 +17,27 @@ app.engine(
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+app.use(express.json());
+
 app.use("/", routes.home);
 app.use("/movies", routes.movies);
 app.use("/contact", routes.contact);
 app.use("/covidinformation", routes.covidinformation);
 
+app.post("/movies/:movieid/reviews", routes.postReviews);
+// res.status(200);
+// console.log(req.body);
+// await fetch('https://lernia-kino-cms.herokuapp.com/api/reviews', {
+//   method: 'POST',
+//   mode: 'cors',
+//   credential: 'same-origin',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(req.body)
+// })
+
 app.use("/static", express.static("./static"));
+
 
 export default app;
