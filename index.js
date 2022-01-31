5de429fff1479d887291e562b7da178c158432e2
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-01-31 22:02:49 +0100

    changed port to 3000.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import app from './src/app.js';
-app.listen(5080);
+/* app.listen(5080); */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
