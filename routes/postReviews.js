b5d65c56271687eb45215f6e8a0a60fd861f87b3
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-02-02 22:45:57 +0100

    added minor validation from client before sending to cms api

--- routes/postReviews.js
+++ routes/postReviews.js
@@ -3,28 +3,6 @@ import express from "express";
 import { LocalStorage } from "node-localstorage";
 import { postData } from "../src/api.js";
 const localStorage = new LocalStorage("./scratch");
-
-// router.post('/reviews', async(req, res) => {
-
-//     const reqToken = req.headers["authorization"].split(":")[1].trim();
-//     let storedToken = localStorage.getItem('token');
-
-//     if (reqToken !== storedToken) {
-//         return res.status(401).end();
-//     }
-
-//     await fetch('https://lernia-kino-cms.herokuapp.com/api/reviews', {
-//         method: 'POST',
-//         mode: 'cors',
-//         credential: 'same-origin',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(req.body)
-//     })
-
-//     res.status(200).end();
-
 const postReview_url = "https://lernia-kino-cms.herokuapp.com/api/reviews";
 
 const router = express.Router();
@@ -38,10 +16,13 @@ router.post("/", async (req, res) => {
     return res.status(401).end();
   }
 
-  postData(postReview_url, req.body).then((data) => {
-    console.log("from postRewur", data);
-    res.status(200).send("test");
-  });
+  if (req.body.data.comment != "" && req.body.data.author != "" && req.body.data.rating != 'number') {
+    postData(postReview_url, req.body).then(() => {
+      return res.status(201).end();
+    })
+  } else {
+    res.status(400).end(); 
+  }
 });
 
 export default router;
