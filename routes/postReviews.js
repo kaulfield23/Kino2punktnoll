55036ba018fb6d2afcbd0441ccdf013d3758028a
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-02-04 00:31:59 +0100

    added a small validation to post request

--- routes/postReviews.js
+++ routes/postReviews.js
@@ -1,45 +1,32 @@
-import fetch from "node-fetch";
-import express from "express";
-import api from "../src/api.js";
-import JWT from "jsonwebtoken";
+import fetch from 'node-fetch';
+import express from 'express';
+import api from '../src/api.js';
+import JWT from 'jsonwebtoken';
 
-const postReview_url = "https://lernia-kino-cms.herokuapp.com/api/reviews";
+const postReview_url = 'https://lernia-kino-cms.herokuapp.com/api/reviews';
 
 const router = express.Router();
 
 //Server logic to handle post from client and pass to CMS API
-router.post("/", async (req, res) => {
-  if (!req.headers["authorization"]) {
+router.post('/', async (req, res) => {
+  if (!req.headers['authorization']) {
     return res.status(401).end();
   }
-  const reqToken = req.headers["authorization"].split(" ")[1];
-  let token = JWT.decode(reqToken, "thisisapassword");
-
-  if (token === null) {
-    return res.status(401).end();
-  }
+  const reqToken = req.headers['authorization'].split(' ')[1];
+  let token = JWT.decode(reqToken, 'thisisapassword');
 
   if (
-    req.body.data.comment != '' &&
-    req.body.data.author != '' &&
-    req.body.data.rating != 'number'
+    Boolean(token !== null) &&
+    Boolean(req.body.data.comment) &&
+    Boolean(req.body.data.author) &&
+    Boolean(req.body.data.rating >= 0 && req.body.data.rating <= 5)
   ) {
-    return res.status(401).end();
+    api.postData(postReview_url, req.body).then(() => {
+      return res.status(201).end();
+    });
   }
-  postData(postReview_url, req.body).then(() => {
-    return res.status(201).end();
-  });
-
-/* 
-  api.postData(postReview_url, req.body).then((data) => {
-    console.log(postReview_url);
-    console.log("from postRewur", data);
-    res.status(200).send("test");
-  }); */
-  /*    res.status(400).end(); */
-
+});
 export default router;
-
 /* 
 Validera recensioner innan de skickas vidare till CMS, så att alla nödvändiga fält är 
 ifyllda och rating är 0-5. 
