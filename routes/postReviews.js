9cfc606a86e06ebdafef4e99809ef58a5bff2a46
Haeju Eom <77925373+kaulfield23@users.noreply.github.com>
2022-02-04 21:08:42 +0100

    Update postReviews.js

--- routes/postReviews.js
+++ routes/postReviews.js
@@ -8,30 +8,25 @@ const postReview_url = 'https://lernia-kino-cms.herokuapp.com/api/reviews';
 const router = express.Router();
 
 //Server logic to handle post from client and pass to CMS API
-router.post('/', async (req, res) => {
-  if (!req.headers['authorization']) {
+//Server logic to handle post from client and pass to CMS API
+router.post("/", async (req, res) => {
+  if (!req.headers["authorization"]) {
     return res.status(401).end();
   }
-  const reqToken = req.headers['authorization'].split(' ')[1];
-  let token = JWT.decode(reqToken, 'thisisapassword');
-  
+  const reqToken = req.headers["authorization"].split(" ")[1];
+  let token = JWT.decode(reqToken, "thisisapassword");
+
   if (token === null) {
-    res.status(401).end();
-  };
-  
-  if (
-    Boolean(token !== null) &&
-    Boolean(req.body.data.comment) &&
-    Boolean(req.body.data.author) &&
-    Boolean(req.body.data.rating >= 0 && req.body.data.rating <= 5)
-  ) {
-    api.postData(postReview_url, req.body).then(() => {
-      return res.status(201).end();
-    });
-  } else {
     return res.status(401).end();
   }
+
+  api.postData(postReview_url, req.body).then((data) => {
+    console.log(postReview_url);
+    console.log("from postRewur", data);
+    res.status(200).send("test");
+  });
 });
+
 export default router;
 /* 
 Validera recensioner innan de skickas vidare till CMS, så att alla nödvändiga fält är 
