ff5542bb39f2e1ac5e5bf49b1239fa61368d403e
kaulfield23 <kaulfield23@gmail.com>
2022-02-04 19:34:46 +0100

    took away useless comments and file

--- src/loginFunc.js
+++ src/loginFunc.js
@@ -2,36 +2,35 @@ import JWT from "jsonwebtoken";
 
 //check if input name and password is matching
 function inUserList(req) {
-  const username = ["richard", "haeju", "johan", "carola", "pontus", "roger"];
-  const password = "groupd";
+    const username = ["richard", "haeju", "johan", "carola", "pontus", "roger"];
+    const password = "groupd";
 
-  const authorization = atob(req.headers["authorization"].slice(6));
-  const inputUserName = authorization.split(":")[0];
-  const inputPassWord = authorization.split(":")[1];
+    const authorization = atob(req.headers["authorization"].slice(6));
+    const inputUserName = authorization.split(":")[0];
+    const inputPassWord = authorization.split(":")[1];
 
-  if (username.includes(inputUserName) && password == inputPassWord) {
-    return inputUserName;
-  } else {
-    return null;
-  }
+    if (username.includes(inputUserName) && password == inputPassWord) {
+        return inputUserName;
+    } else {
+        return null;
+    }
 }
 
 export function loginFunc(req, res) {
-  let username = inUserList(req);
-  if (username) {
-    const token = JWT.sign(
-      {
-        username: username,
-      },
-      "thisisapassword"
-    );
-    //send token to response
-    res.status(200).send({
-      token,
-    });
-  } else {
-    res.status(401).end();
-  }
+    let username = inUserList(req);
+    if (username) {
+        const token = JWT.sign({
+                username: username,
+            },
+            "thisisapassword"
+        );
+        //send token to response
+        res.status(200).send({
+            token,
+        });
+    } else {
+        res.status(401).end();
+    }
 }
 
 export default loginFunc;
